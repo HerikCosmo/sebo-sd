9ece85762204f105499bfdcff8004ca8e8cda006
Dev-Luigy <luigygabriel18a@gmail.com>
2025-06-10 18:38:56 -0300

    refactor: Removes unnused variables.

--- src/communication/LivroOutputStream.java
+++ src/communication/LivroOutputStream.java
@@ -8,65 +8,62 @@ import java.io.OutputStream;
 import java.util.Arrays;
 
 public class LivroOutputStream extends OutputStream {
-    private final Livro[] livros;
-    private final int objectsQuantity;
-    private final OutputStream outputStream;
-    private int currentLivroIndex = 0;
-    private byte[] currentBookBytes = null;
-    private int currentByteIndex = 0;
+  private final Livro[] livros;
+  private final int objectsQuantity;
+  private final OutputStream outputStream;
+  private int currentLivroIndex = 0;
 
-
-    public LivroOutputStream(Livro[] livros, int objectsQuantity, OutputStream outputStream) {
-        if (livros == null || outputStream == null) {
-            throw new IllegalArgumentException("Array de livros e destino não podem ser nulos.");
-        }
-
-        if(objectsQuantity < 0 || objectsQuantity > livros.length) {
-            throw new IllegalArgumentException("Quantidade de objetos é inválido.");
-        }
-
-        this.livros = Arrays.copyOf(livros, livros.length);
-        this.objectsQuantity = objectsQuantity;
-        this.outputStream = outputStream;
+  public LivroOutputStream(Livro[] livros, int objectsQuantity, OutputStream outputStream) {
+    if (livros == null || outputStream == null) {
+      throw new IllegalArgumentException("Array de livros e destino não podem ser nulos.");
     }
 
-    @Override
-    public void write(int b) throws IOException {
-        throw new UnsupportedOperationException("Função Inválida");
+    if (objectsQuantity < 0 || objectsQuantity > livros.length) {
+      throw new IllegalArgumentException("Quantidade de objetos é inválido.");
     }
 
-    public void writeNextBookData() throws IOException {
-        if (currentLivroIndex < objectsQuantity) {
-            Livro livro = livros[currentLivroIndex];
-            DataOutputStream dos = new DataOutputStream(outputStream);
+    this.livros = Arrays.copyOf(livros, livros.length);
+    this.objectsQuantity = objectsQuantity;
+    this.outputStream = outputStream;
+  }
 
-            byte[] idBytes = livro.getId().getBytes("UTF-8");
-            byte[] titleBytes = livro.getTitle().getBytes("UTF-8");
+  @Override
+  public void write(int b) throws IOException {
+    throw new UnsupportedOperationException("Função Inválida");
+  }
 
-            dos.writeInt(idBytes.length);
-            dos.writeInt(titleBytes.length);
-            dos.writeLong(8);
+  public void writeNextBookData() throws IOException {
+    if (currentLivroIndex < objectsQuantity) {
+      Livro livro = livros[currentLivroIndex];
+      DataOutputStream dos = new DataOutputStream(outputStream);
 
-            dos.write(idBytes);
-            dos.write(titleBytes);
-            dos.writeDouble(livro.getPrice());
+      byte[] idBytes = livro.getId().getBytes("UTF-8");
+      byte[] titleBytes = livro.getTitle().getBytes("UTF-8");
 
-            dos.flush();
-            currentLivroIndex++;
-            System.out.println("Livro Enviado: " + livro);
-        } else {
-            System.out.println("Não há dados para ler");
-        }
+      dos.writeInt(idBytes.length);
+      dos.writeInt(titleBytes.length);
+      dos.writeLong(8);
+
+      dos.write(idBytes);
+      dos.write(titleBytes);
+      dos.writeDouble(livro.getPrice());
+
+      dos.flush();
+      currentLivroIndex++;
+      System.out.println("Livro Enviado: " + livro);
+    } else {
+      System.out.println("Não há dados para ler");
     }
+  }
 
-    public void writeAllBooks() throws IOException {
-        while(currentLivroIndex < objectsQuantity) {
-            writeNextBookData();
-        }
+  public void writeAllBooks() throws IOException {
+    while (currentLivroIndex < objectsQuantity) {
+      writeNextBookData();
     }
+  }
 
-    @Override
-    public void close() throws IOException {
-        super.close();
-    }
+  @Override
+  public void close() throws IOException {
+    super.close();
+  }
 }
